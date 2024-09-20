5e3cba44127c9ef1fe3a4aa76ef84208d5721ba5
2001J <josejeth26@gmail.com>
2024-09-20 03:04:18 +0200

    The beginning of the vito trips tour platform

---
+++ src/main/java/com/jethjoe/VitoTripsApplication.java
@@ -0,0 +1,13 @@
+package com.jethjoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitoTripsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitoTripsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jethjoe/VitoTripsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jethjoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitoTripsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
